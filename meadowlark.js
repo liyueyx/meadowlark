880a0e18ec38573a1fca6264c2f51409290d51aa
liyue <liyueyx@126.com>
2017-07-10 13:54:23 +0800

    添加handlarbar

--- meadowlark.js
+++ meadowlark.js
@@ -1,29 +1,41 @@
 
 var express = require('express');
 var app = express();
-app.set('port',process.env.PORT || 3000);
+var exphbs = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars',exphbs.engine)
+app.set('view engine','handlebars');
+
+app.set('port',process.env.PORT || 3000);   //设置服务启动窗口
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
+app.use(express.static(__dirname + '/public'))//设置静态路由
+
 
 app.get('/',function(req,res){
-    res.type('text/plain');
-    res.end('Meadowlark Travel');
+    res.render('home');
 });
 app.get('/about',function(req,res){
-    res.type('text/plain');
-    res.end('about Meadowlark Travel');
+    var randomFortunes = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about',{fortunes:randomFortunes});
 });
 
 //404页面
 app.use(function(req,res){
-    res.type('text/plain');
     res.status(404);
-    res.end('404 not find');
+    res.render('404');
 });
 //500页面
 app.use(function(err,req,res,next){
     console.log(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.end('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
